0546f62ea742e189c52ba5c9cefd889f521a3331
Ishini Hathurusinghe <101358580+ishinihathurusinghe@users.noreply.github.com>
2022-08-17 08:14:57 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,17 @@
+class Swap{
+
+    public static void main(String[] args){
+
+        int a=10, b=20;
+
+        int c;
+
+        c=a;
+        a=b;
+        b=c;
+
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
